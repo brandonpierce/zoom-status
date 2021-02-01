e674ae1e43b51090c4c1808b3cb143e30a9f001c
Brandon Pierce <ihashacks@gmail.com>
2021-01-31 20:16:52 -0800

    add API updates

--- client/status.sh
+++ client/status.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+SERVER="localhost"
+
 status=$(ps -ef | grep -i 'CptHost15980' | grep -v 'grep')
 
 if [[ ${status} ]];
     then
         echo "in a meeting";
+        curl -i -H "Content-Type: application/json" -X PUT -d '{"status":"online"}' "http://${SERVER}:5000/zoom"
     else
         echo "not in a meeting";
+        curl -i -H "Content-Type: application/json" -X PUT -d '{"status":"offline"}' "http://${SERVER}:5000/zoom"
 fi
